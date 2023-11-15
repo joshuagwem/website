816656c1aa3577e4cdefe5fa64bd56743ffe4626
Joshua <joshuagwem@outlook.com>
2023-11-15 15:49:26 -0600

    Heavy overhaul of my website! Included course material for project 11

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -1,13 +1,17 @@
 // For dark mode setting on the project's page
-const body = document.querySelector('body');
-const modeToggle = document.getElementById('mode-toggle');
-const modeRed = document.getElementById('toggle-red');
-const modeStatus = document.querySelector('.mode-status');
+const body = document.querySelector("body");
+const registerDarkMode = document.getElementById("register");
+const modeToggle = document.getElementById("mode-toggle");
+const modeRed = document.getElementById("toggle-red");
+const modeStatus = document.querySelector(".mode-status");
 
 function toggleMode() {
-  body.classList.toggle('dark-mode');
-  modeRed.classList.toggle('redbg');
-  const modeMessage = body.classList.contains('dark-mode') ? 'Dark Mode' : "Light Mode";
+  body.classList.toggle("dark-mode");
+  registerDarkMode.classList.toggle("dark-mode");
+  modeRed.classList.toggle("redbg");
+  const modeMessage = body.classList.contains("dark-mode")
+    ? "Dark Mode"
+    : "Light Mode";
   modeStatus.innerText = "...of nothing!!! Current mode: " + modeMessage;
 }
-modeToggle.addEventListener('click', toggleMode);
+modeToggle.addEventListener("click", toggleMode);
